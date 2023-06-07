ef70dc7fab80b2ea69165b2631efd33b4a3de3f5
Navjot <navjotsandhu910@outlook.com>
2023-06-07 10:25:25 +0530

    initial commit

---
+++ TypeScript/1helloworld.js
@@ -0,0 +1,3 @@
+var user = { name: 'Navjot', age: 10 };
+console.log('Hi Navjot');
+console.log(user.email);
---
+++ TypeScript/1helloworld.ts
@@ -0,0 +1,3 @@
+let user = {name:'Navjot',age:10}
+console.log('Hi Navjot')
+console.log(user.email)
